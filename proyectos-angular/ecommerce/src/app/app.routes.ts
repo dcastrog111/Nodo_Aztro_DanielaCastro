dfc48ca0b8fdc121f91f3e3da93889d7e67dd615
Daniela Castro <dcastrog1@eafit.edu.co>
2024-07-26 11:04:37 -0500

    Creacion proyecto ecommerce

--- proyectos-angular/ecommerce/src/app/app.routes.ts
+++ proyectos-angular/ecommerce/src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"inicio", component :InicioComponent},
+    {path:"tienda", component :TiendaComponent},
+    {path:"nosotros", component :NosotrosComponent},
+];
