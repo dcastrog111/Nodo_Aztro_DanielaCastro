29e228f6a06e2517dea33aeb9bcce50cb515f103
Daniela Castro <dcastrog1@eafit.edu.co>
2024-07-30 10:02:10 -0500

    cambios ecommerce

--- proyectos-angular/ecommerce/src/app/app.routes.ts
+++ proyectos-angular/ecommerce/src/app/app.routes.ts
@@ -4,9 +4,12 @@ import path from 'node:path';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { TiendaComponent } from './pages/tienda/tienda.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
+    {path:"", component:InicioComponent}, //Poner un path vacio para que la pagina por defecto sea inicio
     {path:"inicio", component :InicioComponent},
     {path:"tienda", component :TiendaComponent},
     {path:"nosotros", component :NosotrosComponent},
+    {path: "**", component:NotFoundComponent} //selector universal, para todo lo que no sea lo anterior que está enrutado
 ];
--- proyectos-angular/ecommerce/src/app/pages/inicio/inicio.component.ts
+++ proyectos-angular/ecommerce/src/app/pages/inicio/inicio.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
+
+  // name = "Alfonso";
+
+  // persona = {
+  //   name: "Alejandro",
+  //   correo: "ale@gmail",
+  //   direccion: "calle 10",
+  //   telefono:"310456"
+  // }
+
+  // contadorNum = 0;
+  // contenidoPlace="Escribe un número";
+  // nombreClass = "";
+
+  // incrementar(){
+  //   this.contadorNum = this.contadorNum+1;
+  //   // this.contadorNum++;
+  //   // this.contadorNum+=1;
+  //   if(this.contadorNum%2===0){
+  //     this.nombreClass = "par";
+  //   } else{
+  //     this.nombreClass = "impar";
+  //   }
+  // }
+
+  // reiniciar(){
+  //   this.contadorNum=0;
+  //   this.nombreClass = "reiniciar";
+
+  // }
+
+  // sumar(n1:number,n2:number){
+  //   return n1+n2;
+  // }
+
+  alturaControl = new FormControl;
+  pesoControl = new FormControl;
+  resultado = 0;
+
+  calcularImc(){
+    let altura = this.alturaControl.value;
+    let peso = this.pesoControl.value;
+    let imc = peso * altura;
+
+    this.resultado = imc;
+  }
+
+
 
 }
